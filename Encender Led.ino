626b536a940281408ab2f5f817d002b9a364cf27
Gerardo Pastrana <blackscotts420@gmail.com>
2024-11-05 10:48:01 -0600

    Add files via upload

---
+++ Encender Led.ino
@@ -0,0 +1,22 @@
+const int umbral = 500; 
+const int ledPin = 13; 
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);  
+}
+
+void loop() {
+  int valorLuz = analogRead(pinLDR);
+  Serial.println(valorLuz);
+  
+  // Si el valor de luz es menor que el umbral, encendemos el LED
+  if (valorLuz < umbral) {
+    digitalWrite(ledPin, HIGH); // Encender el LED
+  } else {
+    digitalWrite(ledPin, LOW);  // Apagar el LED
+  }
+  
+  delay(500);
+}
+
